e06b2ca497b1233f811b21f0099fe649c57b5a1d
xujie <xujie@yixia.com>
2020-11-01 18:00:37 +0800

    函子

---
+++ study/day201101/01-functor.js
@@ -0,0 +1,17 @@
+// 函子
+
+class constr{
+  static of (fn){
+    return new constr(fn);
+  }
+  constructor(value){
+    this._value = value
+  }
+  map (fn) {
+    return constr.of(fn(this._value))
+  }
+}
+
+// let r = new constr(3).map(x=>x+1).map(x=>x*8);
+
+console.log(r)
