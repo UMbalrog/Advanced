d263fd2a4ab656f591b59e73584ee069db76c2d5
xujie <302592510@qq.com>
2020-11-13 08:54:43 +0800

    添加文件

--- study/dayTypeScript/10-TypeScript.js
+++ study/dayTypeScript/10-TypeScript.js
@@ -22,7 +22,22 @@ func1(23, 10);
 // 任意类型  
 // any 不会有类型检测
 // 隐式类型推断
-// 假定这个 nums 来自一个明确的接口
+// 类型断言
 var nums = '[110, 120, 119, 112]';
 var num1 = nums;
 var num2 = nums; // JSX 下不能使用
+function printPost(post) {
+    console.log(post.title);
+    console.log(post.content);
+}
+var cache = {};
+cache.foo = 'value1';
+cache.bar = 'value2';
+// 类
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Person;
+}());
