114d39b2a3d4336b21f852fcd8185ea0ceeafb45
xujie <xujie@yixia.com>
2020-12-05 21:35:33 +0800

    eslint

--- study/dayESlint/.eslintrc.js
+++ study/dayESlint/.eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  env: {
+  env: {  // 运行环境，前端一般就是browser和node
     browser: true,
     es2021: true
-  },
-  extends: [
+  },  
+  extends: [  // 继承现有的代码风格，是数组可以添加多个standard比较火
     'standard'
   ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
+  parserOptions: {  //语法解析器的配置
+    ecmaVersion: 12,  //设置ECMAScript版本
+    sourceType: 'module'  //模块儿加载，ESModules
   },
-  rules: {
+  rules: { //具体规则的开启
   }
 }
