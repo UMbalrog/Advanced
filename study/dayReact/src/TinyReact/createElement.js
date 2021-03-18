65ac89341a32d6fe2a958d8daf0861037e7f2541
xujie <xujie@yixia.com>
2021-03-18 19:27:17 +0800

    react

---
+++ study/dayReact/src/TinyReact/createElement.js
@@ -0,0 +1,15 @@
+export default function createElement (type, props, ...children) {  
+  const childElements = [].concat(children).map(child => {
+    if (child instanceof Object){
+      return child
+    } else {
+      return createElement('text', null, { textContent: child })
+    }
+  })
+
+  return {
+    type,
+    props,
+    children: childElements
+  }
+}
