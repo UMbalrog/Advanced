624e71f9966912a5d4424fb83d205f5295744fcf
xujie <xujie@yixia.com>
2021-07-19 19:55:23 +0800

    添加文件

---
+++ study/dayVite/my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+     <h1>{{ msg }}</h1>
+     <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide>slide1</swiper-slide>
+      <swiper-slide>slide2</swiper-slide>
+      <swiper-slide>slide3</swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+import { Swiper, SwiperSlide } from 'wblive-ui';
+import 'swiper/css/swiper.css';
+defineProps({
+  msg: String,
+
+
+
+})
+
+const state = reactive({ count: 0 })
+
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
