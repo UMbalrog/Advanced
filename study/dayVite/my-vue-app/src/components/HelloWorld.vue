b0ded2cc35a9c9eb9eab52fb8c76ddbe7f0a4dd8
xujie <xujie@yixia.com>
2021-08-04 18:54:17 +0800

    测试

--- study/dayVite/my-vue-app/src/components/HelloWorld.vue
+++ study/dayVite/my-vue-app/src/components/HelloWorld.vue
@@ -1,27 +1,51 @@
 <template>
   <div>
-     <h1>{{ msg }}</h1>
-     <swiper ref="mySwiper" :options="swiperOptions">
-      <swiper-slide>slide1</swiper-slide>
-      <swiper-slide>slide2</swiper-slide>
-      <swiper-slide>slide3</swiper-slide>
-    </swiper>
+    <h1>{{ msg }}</h1>
+    <div>数字{{state.count}}</div>
+    <div @click="setCount">按钮</div>
+    <div>····················</div>
+    <div>
+      <div v-for="(item, k) in list" :key="k">
+        <span>{{item.name}}</span><span>{{item.age}}</span>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
-import { Swiper, SwiperSlide } from 'wblive-ui';
-import 'swiper/css/swiper.css';
+import { defineProps, reactive, onBeforeMount } from 'vue';
+
 defineProps({
   msg: String,
-
-
-
 })
+let age = 60;
 
-const state = reactive({ count: 0 })
+const state = reactive({ count: 0 });
+const list = reactive([
+  {
+    name: "张三",
+    age: 80
+  }
+]);
+const setCount = () => {
+  state.count ++;
+}
 
+const autoPush = () => {
+  console.log(age)
+  setTimeout(() => {
+    list.push({
+      name: "赵六",
+      age: age++
+    });
+    // autoPush();
+  },2000)
+}
+
+onBeforeMount(() => {
+  autoPush();
+})
 
 </script>
 
