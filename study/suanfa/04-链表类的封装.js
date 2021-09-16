46c50cbe94a50f5def949e59e9dc64ef5df5ada8
xujie <302592510@qq.com>
2021-09-16 21:25:27 +0800

    添加

--- study/suanfa/04-链表类的封装.js
+++ study/suanfa/04-链表类的封装.js
@@ -18,11 +18,72 @@ class LinkedList {
   // 添加节点头部
   addAtHead(value){
     let node = new LinkedNode(value);
-    node.next = this.head;
-    this.head = node;
+    if(this.count == 0){
+      this.head = node;
+    }else{
+      node.next = this.head;
+      this.head = node;
+    }
     this.count++;
+  }
+  // 添加节点尾部
+  addAtTail(value){
+    let node = new LinkedNode(value);
+    if(this.count == 0){
+      this.head = node;
+    }else{
+      let cur = this.head;
+      while(cur.next != null){
+        cur = cur.next; 
+      }
+      cur.next = node;
+    }
+    this.count++;
+  }
+  // 获取节点（根据索引）
+  get(index) { // 0 --- this.count-1
+    if(index < 0){return}
+    if(index >= this.count){return}
+    let node = this.head;
+    for(let i=0; i<index; i++){
+      node = node.next
+    }
+    return node;
+  }
+  // 添加节点（根据索引）
+  addAtIndex (value, index) {
+    if(index <= 0 ){
+      return this.addAtHead(value);
+    }
+    if(index >= this.count ){
+      return this.addAtTail(value);
+    }
+    let prew = this.get(index-1);
+    let node = new LinkedNode(value);
+    node.next = prew.next;
+    prew.next = node;
+    this.count++;
+  }
+  // 删除节点
+  removeAtIndex (index) {
+    if(index <= 0 ){
+      this.head = this.head.next;
+      this.count--;
+      return;
+    }
+    if(index >= this.count ){
+      console.log('不做删除');
+      return;
+    }
+    let prew = this.get(index-1);
+    prew.next = prew.next.next;
+    this.count--;
   }
 } 
 
 
 let l = new LinkedList();
+l.addAtTail('a')
+l.addAtTail('b')
+l.addAtTail('c')
+l.addAtTail('d')
