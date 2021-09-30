3f7b5eef205044be6fc236ed261c00c6889348a3
xujie3 <>
2021-09-30 16:01:41 +0800

    file

--- study/node/05-file-api.js
+++ study/node/05-file-api.js
@@ -9,13 +9,26 @@ const path = require('path');
 //   }
 // })
 
-fs.writeFile('data.txt', '111', {
-  mode: 438,
-  flag: 'w+',
-  encoding: 'utf-8'
-}, (err) => {
-  if(err){
-    console.log(err);
+// fs.writeFile('data.txt', '111', {
+//   mode: 438,
+//   flag: 'w+',
+//   encoding: 'utf-8'
+// }, (err) => {
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log('我写入了')
+// })
+
+// copyFile
+// fs.copyFile('data.txt', 'test.txt', () => {
+//   console.log('复制完成')
+// })
+
+// watchFile
+fs.watchFile('data.txt', {interval: 20}, (curr, prev) => {
+  console.log('文件被修改了');
+  if(curr.mtime !== prev.mtime){ //监测是否变化
+    fs.unwatchFile('data.txt');
   }
-  console.log('我写入了')
 })
